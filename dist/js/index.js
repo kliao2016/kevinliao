9639b660fb0eaa40a271d47f78cb934945673346
Kevin Liao <kliao2016@gmail.com>
2020-10-11 20:03:32 -0600

    Hide navbar on scroll on mobile

--- dist/js/index.js
+++ dist/js/index.js
@@ -11,7 +11,9 @@ function setupSmoothScroll() {
         
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            
+            // Hide navbar on mobile
+            $('.navbar-collapse').collapse('hide');
+
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - 70
